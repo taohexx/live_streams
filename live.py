9a0357a0e978b27dc88f5be17d396a7e0922d3d0
马赛克 <wanglaihuai@gmail.com>
2017-06-13 17:21:25 +0800

    Update live.py

--- live.py
+++ live.py
@@ -1,11 +1,11 @@
 # coding:utf-8
 import requests
-def postdata():
-    url='http://www.wipo360.com/Api/SiSi/getLive'
-    data={'timestamp':'1493473697','limit_begin':0,'version_num':'2.0.0','limit_num':20,'os':'android','sign':'fd9ad9163eb1bac3bdd84c7b416ffbe6','soft_ver':'4.2.2','type':2}
-    res=requests.post(url,data=data).json()
-    #print(res['info'])
-    for item in res['info']:
-        print(item['channel_source'])
+def getlist():
+    url='http://www.xvsite.com/api/public/?service=Home.getHot'
+    res=requests.get(url).json()
+    data=res['data']['info'][0]['list']
+    for info in data:
+        print(info['pull'])
+
 if __name__=="__main__":
-    postdata()
+    getlist()
